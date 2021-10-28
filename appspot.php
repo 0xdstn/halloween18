c6e4ed3ba0d85f9c3a116ee67ae8592d61165635
Dustin <0xdstn@protonmail.com>
2021-10-28 16:06:51 -0700

    Initial commit

---
+++ appspot.php
@@ -0,0 +1,7 @@
+<?php var_dump($_GET); ?>
+google.doodle.lsc.c1({
+  "kind": "urlshortener#url", 
+  "longUrl": "https://www.google.com/webhp?doodle=73509581_CiQ3ODcxMzcyNC0zYjExLTRiYmItYjc5Yi05ODBjZTg1ZGYxMTkiMGhhbGxvd2VlbjE4LXVzLWNlbnRyYWwxLWYuaDE4LmNsb3VkLmRvb2RsZXMuZ29vZw..", 
+  "id": "//g.co/doodle/72dvttx", 
+  "status": "OK"
+})
